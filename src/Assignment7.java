7802ca80cd6e78cae458d4c80d8776e8c578d941
SuzannaO <ostrovskaya.s3@gmail.com>
2021-10-16 20:48:54 +0300

    initial commit

---
+++ src/Assignment7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Assignment7 {
+    public static void main(String args[]) {
+
+        System.out.println("Please Enter a number : ");
+        int number = new Scanner(System.in).nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println("Number : " + number + " is a palindrome");
+        } else {
+            System.out.println("Number : " + number + " is not a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
